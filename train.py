962fbe2b2d3b0d29740ee0c8cbaa6f940ba2f8ff
Yash Upadhyay <yash256@gmail.com>
2021-05-06 05:56:35 +0000

    Hyperdrive v1 changes

--- train.py
+++ train.py
@@ -14,13 +14,12 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+path_to_data = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=path_to_data)
 
 x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=11)
 
 run = Run.get_context()
 
